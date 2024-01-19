438e47655da4cc2f2035e858492ef64059ff1f7c
Laman Khudadatzada <lkhudadatzada@gmail.com>
2024-01-19 12:00:03 +0400

    "customer entity & adding lombok dependency"

---
+++ customer_ms/src/main/java/com/namanx/customer_ms/model/entity/Customer.java
@@ -0,0 +1,24 @@
+package com.namanx.customer_ms.model.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String firstName;
+    String lastName;
+    String email;
+}
