ca52ac21b3e09b2600284c275f2ba9a6fc17c587
Laman Khudadatzada <lkhudadatzada@gmail.com>
2024-01-23 22:26:34 +0400

    "creating fraud check history"

---
+++ fraud_ms/src/main/java/com/namanx/fraud_ms/FraudCheckHistory.java
@@ -0,0 +1,16 @@
+package com.namanx.fraud_ms;
+
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class FraudCheckHistory {
+}
